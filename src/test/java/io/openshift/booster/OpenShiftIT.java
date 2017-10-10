cfa251df8eca0348d376fb4233f72d1d6682a821
Martin Spisiak <mspisiak@redhat.com>
2017-10-10 09:42:11 +0200

    Reworked integration tests to use Arquillian Cube instead of OTA

--- src/test/java/io/openshift/booster/OpenShiftIT.java
+++ src/test/java/io/openshift/booster/OpenShiftIT.java
@@ -1,16 +1,18 @@
 package io.openshift.booster;
 
+import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import io.fabric8.kubernetes.api.model.ConfigMap;
-import io.openshift.booster.test.OpenShiftTestAssistant;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import io.fabric8.openshift.client.OpenShiftClient;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.*;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
-import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static com.jayway.awaitility.Awaitility.await;
@@ -22,28 +24,24 @@ import static org.hamcrest.core.IsEqual.equalTo;
  * Check the behavior of the application when running in OpenShift.
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(Arquillian.class)
 public class OpenShiftIT {
 
-    private static OpenShiftTestAssistant assistant = new OpenShiftTestAssistant();
+    private final String applicationName = "configmap-vertx";
 
-    @BeforeClass
-    public static void prepare() throws Exception {
-        assistant.deployApplication();
-        assistant.deploy("app-config", new File("target/test-classes/test-config.yml"));
+    @RouteURL(applicationName)
+    private URL route;
 
-    }
-    @AfterClass
-    public static void cleanup() {
-        try {
-            assistant.cleanup();
-        } catch (Exception e) {
-            // Ignore it.
-        }
+    @ArquillianResource
+    private OpenShiftClient oc;
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = route.toString();
     }
 
     @Test
     public void testAThatWeAreReady() throws Exception {
-        assistant.awaitApplicationReadinessOrFail();
         await().atMost(5, TimeUnit.MINUTES).catchUncaughtExceptions().until(() -> {
             Response response = get();
             return response.getStatusCode() < 500;
@@ -58,10 +56,10 @@ public class OpenShiftIT {
 
     @Test
     public void testCThatWeCanReloadTheConfiguration() {
-        ConfigMap map = assistant.client().configMaps().withName("app-config").get();
+        ConfigMap map = oc.configMaps().withName("app-config").get();
         assertThat(map).isNotNull();
 
-        assistant.client().configMaps().withName("app-config").edit()
+        oc.configMaps().withName("app-config").edit()
             .addToData("app-config.yml", "message : \"Bonjour, %s from a ConfigMap !\"")
             .done();
 
@@ -74,7 +72,7 @@ public class OpenShiftIT {
     @Test
     public void testDThatWeServeErrorWithoutConfigMap() {
         get("/api/greeting").then().statusCode(200);
-        assistant.client().configMaps().withName("app-config").delete();
+        oc.configMaps().withName("app-config").delete();
 
         await().atMost(5, TimeUnit.MINUTES).catchUncaughtExceptions().until(() ->
                 get("/api/greeting").then().statusCode(500)
